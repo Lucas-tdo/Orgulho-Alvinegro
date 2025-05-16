0416892e8132fc168b8c3ad5d20fe91389ffd801
Lucas-tdo <lucas.santos114@sptech.school>
2025-05-16 16:14:02 -0300

    Adicionando camisas e arrumando sql

--- Documentos/Banco de Dados/2versao/códigosql2.sql
+++ Documentos/Banco de Dados/2versao/códigosql2.sql
@@ -1,5 +1,6 @@
 create database orgulho_alvinegro;
 use orgulho_alvinegro;
+drop database orgulho_alvinegro;
 
 create table usuario (
 idusuario int primary key auto_increment,
@@ -32,8 +33,45 @@ ano year
 insert into camisa values
 (default,'1° Camisa','image/camisas/camisa2024-1.png','219.90','Reebook','2024'),
 (default,'2° Camisa','image/camisas/camisa2024-2.png','219.90','Reebook','2024'),
-(default,'3° Camisa','image/camisas/camisa2024-3.png','279.90','Reebook','2024');
-
+(default,'3° Camisa','image/camisas/camisa2024-3.png','279.90','Reebook','2024'),
+(default,'1° Camisa','image/camisas/camisa2023-1.png','129.90','Reebook','2023'),
+(default,'2° Camisa','image/camisas/camisa2023-2.png','239.90','Reebook','2023'),
+(default,'1° Camisa','image/camisas/camisa2022-1.png','129.90','Reebook','2022'),
+(default,'4° Camisa','image/camisas/camisa2022-4.png','179.90','Reebook','2022'),
+(default,'3° Camisa','image/camisas/camisa2021-3.png','169.90','Kappa','2021'),
+(default,'4° Camisa','image/camisas/camisa2021-4.png','189.90','Kappa','2021'),
+(default,'1° Camisa','image/camisas/camisa2020-1.png','225.30','Kappa','2020'),
+(default,'3° Camisa','image/camisas/camisa2019-3.png','169.90','Kappa','2019'),
+(default,'1° Camisa','image/camisas/camisa2017-1.png','169.90','Topper','2017'),-- não sei preço
+(default,'3° Camisa','image/camisas/camisa2017-3.png','169.90','Topper','2017'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa2016-1.png','169.90','Topper','2016'),-- não sei preço
+(default,'4° Camisa','image/camisas/camisa2016-4.png','169.90','Topper','2016'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa2015-1.png','169.90','Puma','2015'),-- não sei preço
+(default,'2° Camisa','image/camisas/camisa2015-2.png','169.90','Puma','2015'),-- não sei preço
+(default,'3° Camisa','image/camisas/camisa2014-3.png','169.90','Puma','2014'),-- não sei preço
+(default,'4° Camisa','image/camisas/camisa2014-4.png','169.90','Puma','2014'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa2013-1.png','169.90','Puma','2013'),-- não sei preço
+(default,'2° Camisa','image/camisas/camisa2013-2.png','169.90','Puma','2013'),-- não sei preço
+(default,'3° Camisa','image/camisas/camisa2013-3.png','169.90','Puma','2013'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa2012-1.png','169.90','Puma','2012'),-- não sei preço
+(default,'2° Camisa','image/camisas/camisa2012-2.png','169.90','Puma','2012'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa2008-1.png','169.90','Kappa','2008'),-- não sei preço
+(default,'2° Camisa','image/camisas/camisa2008-2.png','169.90','Kappa','2008'),-- não sei preço
+(default,'3° Camisa','image/camisas/camisa2008-3.png','169.90','Kappa','2008'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa2006-1.png','169.90','Kappa','2006'),-- não sei preço
+(default,'2° Camisa','image/camisas/camisa2006-2.png','169.90','Kappa','2006'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa2002-1.png','169.90','Finta','2002'),-- não sei preço
+(default,'2° Camisa','image/camisas/camisa2002-2.png','169.90','Finta','2002'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa1997-1.png','169.90','Finta','1997'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa1996-1.png','169.90','Finta','1996'),-- não sei preço
+(default,'3° Camisa','image/camisas/camisa1996-3.png','169.90','Finta','1996'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa1995-1.png','169.90','Finta','1995'),-- não sei preço
+(default,'2° Camisa','image/camisas/camisa1995-2.png','169.90','Finta','1995'),-- não sei preço
+(default,'3° Camisa','image/camisas/camisa1995-3.png','169.90','Finta','1995'),-- não sei preço
+(default,'1° Camisa','image/camisas/camisa1988-1.png','169.90','Finta','1988'),-- não sei preço
+(default,'2° Camisa','image/camisas/camisa1988-2.png','169.90','Finta','1988'),-- não sei preço
+(default,'2° Camisa','image/camisas/camisa1962-2.png','169.90','','1962'),-- não sei preço
+(default,'3° Camisa','image/camisas/camisa1962-3.png','169.90','','1962');-- não sei preço
 
 select nome,imagem,preco,marca,ano from camisa;
 
@@ -47,7 +85,7 @@ foreign key(idusuario) references usuario (idusuario),
 foreign key(idcamisa) references camisa(idcamisa)
 );
 
-insert into favoritas values(2,4);
+
 create table idolo(
 ididolo int primary key auto_increment,
 nome varchar(45),
@@ -71,6 +109,7 @@ primary key(ididolo,idtitulo),
 foreign key (ididolo) references idolo(ididolo),
 foreign key (idtitulo) references titulo(idtitulo)
 );
+
 
 select * from usuario;
 
