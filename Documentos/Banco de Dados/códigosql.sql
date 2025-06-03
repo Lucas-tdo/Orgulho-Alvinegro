8a9723f660262d044006d95762c742a57f141b40
Lucas-tdo <lucas.santos114@sptech.school>
2025-06-02 23:16:13 -0300

    Adicionando descrição aos títulos

--- Documentos/Banco de Dados/códigosql.sql
+++ Documentos/Banco de Dados/códigosql.sql
@@ -40,36 +40,36 @@ insert into camisa values
 (default,'4° Camisa','image/camisas/camisa2021-4.png','189.90','Kappa','2021'),
 (default,'1° Camisa','image/camisas/camisa2020-1.png','225.30','Kappa','2020'),
 (default,'3° Camisa','image/camisas/camisa2019-3.png','169.90','Kappa','2019'),
-(default,'1° Camisa','image/camisas/camisa2017-1.png','169.90','Topper','2017'),-- não sei preço
-(default,'3° Camisa','image/camisas/camisa2017-3.png','169.90','Topper','2017'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa2016-1.png','169.90','Topper','2016'),-- não sei preço
-(default,'4° Camisa','image/camisas/camisa2016-4.png','169.90','Topper','2016'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa2015-1.png','169.90','Puma','2015'),-- não sei preço
-(default,'2° Camisa','image/camisas/camisa2015-2.png','169.90','Puma','2015'),-- não sei preço
-(default,'3° Camisa','image/camisas/camisa2014-3.png','169.90','Puma','2014'),-- não sei preço
-(default,'4° Camisa','image/camisas/camisa2014-4.png','169.90','Puma','2014'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa2013-1.png','169.90','Puma','2013'),-- não sei preço
-(default,'2° Camisa','image/camisas/camisa2013-2.png','169.90','Puma','2013'),-- não sei preço
-(default,'3° Camisa','image/camisas/camisa2013-3.png','169.90','Puma','2013'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa2012-1.png','169.90','Puma','2012'),-- não sei preço
-(default,'2° Camisa','image/camisas/camisa2012-2.png','169.90','Puma','2012'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa2008-1.png','169.90','Kappa','2008'),-- não sei preço
-(default,'2° Camisa','image/camisas/camisa2008-2.png','169.90','Kappa','2008'),-- não sei preço
-(default,'3° Camisa','image/camisas/camisa2008-3.png','169.90','Kappa','2008'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa2006-1.png','169.90','Kappa','2006'),-- não sei preço
-(default,'2° Camisa','image/camisas/camisa2006-2.png','169.90','Kappa','2006'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa2002-1.png','169.90','Finta','2002'),-- não sei preço
-(default,'2° Camisa','image/camisas/camisa2002-2.png','169.90','Finta','2002'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa1997-1.png','169.90','Finta','1997'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa1996-1.png','169.90','Finta','1996'),-- não sei preço
-(default,'3° Camisa','image/camisas/camisa1996-3.png','169.90','Finta','1996'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa1995-1.png','169.90','Finta','1995'),-- não sei preço
-(default,'2° Camisa','image/camisas/camisa1995-2.png','169.90','Finta','1995'),-- não sei preço
-(default,'3° Camisa','image/camisas/camisa1995-3.png','169.90','Finta','1995'),-- não sei preço
-(default,'1° Camisa','image/camisas/camisa1988-1.png','169.90','Finta','1988'),-- não sei preço
-(default,'2° Camisa','image/camisas/camisa1988-2.png','169.90','Finta','1988'),-- não sei preço
-(default,'2° Camisa','image/camisas/camisa1962-2.png','169.90','','1962'),-- não sei preço
-(default,'3° Camisa','image/camisas/camisa1962-3.png','169.90','','1962');-- não sei preço
+(default,'1° Camisa','image/camisas/camisa2017-1.png','200.90','Topper','2017'),
+(default,'3° Camisa','image/camisas/camisa2017-3.png','140.90','Topper','2017'),
+(default,'1° Camisa','image/camisas/camisa2016-1.png','250.90','Topper','2016'),
+(default,'4° Camisa','image/camisas/camisa2016-4.png','120.90','Topper','2016'),
+(default,'1° Camisa','image/camisas/camisa2015-1.png','200.90','Puma','2015'),
+(default,'2° Camisa','image/camisas/camisa2015-2.png','200.90','Puma','2015'),
+(default,'3° Camisa','image/camisas/camisa2014-3.png','220.90','Puma','2014'),
+(default,'4° Camisa','image/camisas/camisa2014-4.png','180.90','Puma','2014'),
+(default,'1° Camisa','image/camisas/camisa2013-1.png','220.90','Puma','2013'),
+(default,'2° Camisa','image/camisas/camisa2013-2.png','200.90','Puma','2013'),
+(default,'3° Camisa','image/camisas/camisa2013-3.png','200.90','Puma','2013'),
+(default,'1° Camisa','image/camisas/camisa2012-1.png','240.90','Puma','2012'),
+(default,'2° Camisa','image/camisas/camisa2012-2.png','170.90','Puma','2012'),
+(default,'1° Camisa','image/camisas/camisa2008-1.png','189.90','Kappa','2008'),
+(default,'2° Camisa','image/camisas/camisa2008-2.png','169.90','Kappa','2008'),
+(default,'3° Camisa','image/camisas/camisa2008-3.png','139.90','Kappa','2008'),
+(default,'1° Camisa','image/camisas/camisa2006-1.png','140.90','Kappa','2006'),
+(default,'2° Camisa','image/camisas/camisa2006-2.png','130.90','Kappa','2006'),
+(default,'1° Camisa','image/camisas/camisa2002-1.png','230.90','Finta','2002'),
+(default,'2° Camisa','image/camisas/camisa2002-2.png','190.90','Finta','2002'),
+(default,'1° Camisa','image/camisas/camisa1997-1.png','190.90','Finta','1997'),
+(default,'1° Camisa','image/camisas/camisa1996-1.png','180.90','Finta','1996'),
+(default,'3° Camisa','image/camisas/camisa1996-3.png','110.90','Finta','1996'),
+(default,'1° Camisa','image/camisas/camisa1995-1.png','270.90','Finta','1995'),
+(default,'2° Camisa','image/camisas/camisa1995-2.png','250.90','Finta','1995'),
+(default,'3° Camisa','image/camisas/camisa1995-3.png','250.90','Finta','1995'),
+(default,'1° Camisa','image/camisas/camisa1988-1.png','189.90','Finta','1988'),
+(default,'2° Camisa','image/camisas/camisa1988-2.png','165.90','Finta','1988'),
+(default,'2° Camisa','image/camisas/camisa1962-2.png','270.90','','1962'),
+(default,'3° Camisa','image/camisas/camisa1962-3.png','300.90','','1962');
 
 create table favoritas(
 idusuario int,
@@ -88,32 +88,32 @@ ano year
 );
 
 insert into titulo values
-(default, "Campeonato Brasileiro", "image/titulos/campeonatobrasileiro24.png", "desc", "2024"),
-(default, "Libertadores", "image/titulos/libertadores.png", "desc", "2024"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "2018"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "2013"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "2010"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "2006"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1997"),
-(default, "Campeonato Brasileiro", "image/titulos/campeonatobrasileiro95.png", "desc", "1995"),
-(default, "Copa Conmebol", "image/titulos/copaconmebol.png", "desc", "1993"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1990"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1989"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1968"),
-(default, "Campeonato Brasileiro", "image/titulos/campeonatobrasileiro68.png", "desc", "1968"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1967"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1962"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1961"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1957"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1948"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1935"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1934"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1933"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1932"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1930"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1912"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1910"),
-(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "desc", "1907");
+(default, "Campeonato Brasileiro", "image/titulos/campeonatobrasileiro24.png", "O terceiro título brasileiro do Botafogo. Com o time com uma campanha histórica, após vitória de 2x1 contra o São Paulo. Com gols de Savariono e Gregore.", "2024"),
+(default, "Libertadores", "image/titulos/libertadores.png", "O primeiro título de Libertadores do Botafogo. Com o time ganhando em cima do Atlético Mineiro por 3x1. Com gols de Luiz Henrique, Alex Telles e Junior Santos.", "2024"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O vigésimo primeiro título carioca do Botafogo . Com o time ganhando em cima do Vasco nos pênaltis após empate em 1x1. Com gol de Joel Carli no tempo normal e defesas decisivas de Gatito Fernández nos pênaltis.", "2018"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O vigésimo título carioca do Botafogo. Com o time ganhando em cima do Fluminense por 1x0. Com gol de Rafael Marques.", "2013"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo nono título carioca do Botafogo Com o time ganhando em cima do Flamengo por 2x1. Com gols de Herrera e Abreu.", "2010"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo oitavo título carioca do Botafogo . Com o time ganhando em cima do Madureira por 3x1. Com dois gols de Dodô e Reinaldo.", "2006"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo sétimo título carioca do Botafogo . Com o time ganhando em cima do Vasco por 1x0. Com gol de Dimba.", "1997"),
+(default, "Campeonato Brasileiro", "image/titulos/campeonatobrasileiro95.png", "O segundo título brasileiro do Botafogo. Com o time ganhando em cima do Santos por 1x1 no segundo jogo, após vitória de 2x1 no primeiro. Com dois gols de Túlio Maravilha no primeiro jogo e Maurício no segundo.", "1995"),
+(default, "Copa Conmebol", "image/titulos/copaconmebol.png", "O primeiro título da Copa Conmebol do Botafogo. Com o time vencendo o Peñarol nos penaltis, após empate por 1x1 no primeiro e 2x2 no segundo. Com gol de Perivaldo no primeiro jogo e gols de Eliel e Sinval no segundo.", "1993"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo sexto título carioca do Botafogo . Com o time ganhando em cima do Vasco por 1x0. Com gol de Carlos Alberto Dias.", "1990"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo quinto título carioca do Botafogo. Com o time ganhando em cima do Flamengo por 1x0. Com gol de Maurício", "1989"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo quarto título carioca do Botafogo . Com o time ganhando em cima do Vasco por 4x0. Com gols de Jairzinho, Gerson , Roberto Miranda e Paulo César.", "1968"),
+(default, "Campeonato Brasileiro", "image/titulos/campeonatobrasileiro68.png", " O primeiro título brasileiro do Botafogo . Com o time ganhando em cima do Fortaleza por 4x0. Com dois gols de Ferretti, um de Roberto Miranda e um de Paulo César.", "1968"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo terceiro título carioca do Botafogo. Com o time ganhando em cima do Bangu por 2x1. Com gols de Roberto Miranda e Gérson.", "1967"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo segundo título carioca do Botafogo . Com o time ganhando em cima do Flamengo por 3x0. Com dois gols de Garrincha e um contra de Vanderlei.", "1962"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo primeiro título carioca do Botafogo. Com o time ganhando em cima do Flamengo por 3x0. Com dois gols de Amarildo e um de Luís Antônio.", "1961"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O décimo título carioca do Botafogo . Com o time ganhando em cima do Fluminense por 6x2. Com cinco gols de Paulinho Valentim e um Garrincha.", "1957"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O nono título carioca do Botafogo . Com o time ganhando em cima do Vasco por 3x1. Com gols de Paraguaio, Octávio e Braguinha.", "1948"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O oitavo título carioca do Botafogo . Com o time ganhando em cima do Andarahy por 5x4. Com dois gols de Álvaro, um de Patesko,um de Carvalho Leite e um de Russinho.", "1935"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", " O sétimo título carioca do Botafogo . Com o time ganhando em cima do Andarahy por 2x1. Com gols de Nilo e Áttila.", "1934"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O sexto título carioca do Botafogo . Com o time se sagrando campeão contra o Olaria em um empate de 1x1. Com gol de Áttila.", "1933"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", " O quinto título carioca do Botafogo . Com o time se sagrando campeão contra o Bonsucesso em uma vitória de 5x4. Com quatro gols de Nilo e um de Paulinho.", "1932"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O quarto título carioca do Botafogo . Com o time se sagrando campeão contra o Fluminense em um empate de 2x2. Com gols de Nilo e Celso.", "1930"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O terceiro título carioca do Botafogo . Com o time se sagrando campeão contra o Germânia em uma vitória de 1x0. Com gol de Mimi Sodré", "1912"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O segundo título carioca do Botafogo . Com o time se sagrando campeão contra o Fluminense em uma vitória de 6x1.Com três gols de Abelardo de Lamare e dois de Décio Viccari e um de Mimi Sodré.", "1910"),
+(default, "Campeonato Carioca", "image/titulos/campeonatocarioca.png", "O primeiro título carioca do Botafogo", "1907");
 
 
 
